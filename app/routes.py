7d0f52756037bb31df9857a514c4620cbc831956
Jonas <erthaljonas@gmail.com>
2019-02-17 13:59:48 -0500

    Created /image path

--- app/routes.py
+++ app/routes.py
@@ -3,7 +3,11 @@ from flask import request, jsonify
 
 from api.api_caller import *
 
-@app.route('/', methods=['GET'])
+@app.route('/')
+def index():
+    return "WELCOME to JONAS OF THRONES! Search with the '/image?name=' query to get back a image URL for your character" 
+
+@app.route('/image', methods=['GET'])
 def get_image():
     char_name = request.args.get("name")
     print(char_name)
